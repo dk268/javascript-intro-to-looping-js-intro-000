c27c41131324cc9bf778f91dde3fee095604b346
Daniel Kerr <lorddanp@aol.com>
2017-11-09 05:46:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(loopTheory){
-  for (var i = 0; i < 25; i++){
+/*  for (var i = 0; i < 25; i++){
     if (i = 1){
       loopTheory[i] = "I am 1 strange loop.";
     }
     else{
     loopTheory[i] = `I am ${i} strange loops.`;
     }
-  }
+  }*/
   return loopTheory;
 }
 
@@ -21,7 +21,7 @@ function whileLoop(num) {
 return Math.random() >= 0.5
 } */
 
-/*function doWhileLoop(array) {
+function doWhileLoop(array) {
   var i = 0;
   var r;
 do {
@@ -29,4 +29,4 @@ do {
   i++;
   r = Math.Random();
 } while (r >= 0.5 && doItOnce.length > 0);
-} */
+}
